48ac0c68dfba2efc4706adb2fa1fedace6984546
Dien <dien25888@gmail.com>
2020-03-16 21:00:54 +0700

    Xpath

---
+++ FirsttestXml/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirsttestXml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtbox.Text = Xuly.test();
+            txtbox.Text += "Những sinh viên có tuổi 22 \n";
+            txtbox.Text += Xuly.tuoi();
+        }
+    }
+}
---
+++ FirsttestXml/Xuly.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+namespace FirsttestXml
+{
+    public class Xuly
+    {        
+        public static string test()
+        {
+            string txt = " ";
+            XmlDocument doc = new XmlDocument();
+            string filename = @"C:\Users\ADMIN\Desktop\Train C#\FirsttestXml\FirsttestXml\sinhvien.xml";
+            doc.Load(filename);
+
+            XmlNode sv = doc.SelectSingleNode("/lophoc");
+
+            foreach(XmlElement item in sv.ChildNodes)
+            {
+                txt += item.InnerText +" "+ item.GetAttribute("id")+"\n";
+            }
+
+            return txt;
+        }
+
+        public static string tuoi()
+        {
+            string txt = " ";
+            XmlDocument doc = new XmlDocument();
+            string filename = @"C:\Users\ADMIN\Desktop\Train C#\FirsttestXml\FirsttestXml\sinhvien.xml";
+            doc.Load(filename);
+            XmlNodeList s = doc.SelectNodes("/lophoc/sinhvien[tuoi=22]/ten");
+            foreach (XmlElement item in s)
+            {
+                txt += item.InnerText + "\n";
+            }
+
+            return txt;
+        }
+        
+    }
+}
